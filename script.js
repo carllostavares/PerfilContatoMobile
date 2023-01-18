b5e823d0e7cff8d8af3d4825fd705cf00a9fee0d
Carlos Tavares <carlostavares2350@gmail.com>
2023-01-18 18:47:14 -0300

    Adicionando img README

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 function toggleMode(){
     
     const html = document.documentElement
+    html.classList.toggle('light')//TODA O IF DE baixo FOI TROCADA PELO '.TOGGLE'
 
     /*if(html.classList.contains('light')){
         html.classList.remove('light')
     }else{
         html.classList.add('light')
-    }
 */
 
-html.classList.toggle('light');//TODA O IF DE CIMA FOI TROCADA PELO '.TOGGLE'
-}
-
 //PEGAR A IMAGEM
+const img = document.querySelector("#profile img")
 
 //SUBSTITUIR A IMAGGEM
-
+if(html.classList.contains('light')) {
 //SE TIVER LIGHT MODE, AADICIONAR A IMAGEM LIGHT
+img.setAttribute('src', './assets/imgPerfilLight.jpg')
 
+}else{
+    img.setAttribute('src', './assets/imgPerfil.jpg')
+}
+}
 //SE TIVER SEM O LIGHT MODE, MANTER A IMAGEM NORMAAL
