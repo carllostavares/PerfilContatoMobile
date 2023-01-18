17f4982f91a472fcf2639ce8bf6e8b3af047fc89
Carlos Tavares <carlostavares2350@gmail.com>
2023-01-18 16:30:06 -0300

    Adicionado button para mudar de modo  dia para noite juntamente com o auxílio da interação JavaScript atravez de uma função chamada toggleMode.

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode(){
+    
+    const html = document.documentElement
+
+    /*if(html.classList.contains('light')){
+        html.classList.remove('light')
+    }else{
+        html.classList.add('light')
+    }
+*/
+
+html.classList.toggle('light');//TODA O IF DE CIMA FOI TROCADA PELO '.TOGGLE'
+}
+
+//PEGAR A IMAGEM
+
+//SUBSTITUIR A IMAGGEM
+
+//SE TIVER LIGHT MODE, AADICIONAR A IMAGEM LIGHT
+
+//SE TIVER SEM O LIGHT MODE, MANTER A IMAGEM NORMAAL
