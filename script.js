8cb1c1da419749159a4e43b764c2cba4bd1c0ae0
Carlos Tavares <carlostavares2350@gmail.com>
2023-03-27 12:46:33 -0300

    Corrigindo erro da troca da foto.

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ const img = document.querySelector("#profile img")
 
 //SUBSTITUIR A IMAGGEM
 if(html.classList.contains('light')) {
-//SE TIVER LIGHT MODE, AADICIONAR A IMAGEM LIGHT
+//SE TIVER LIGHT MODE, ADICIONAR A IMAGEM LIGHT
 img.setAttribute('src', './assets/imgPerfilLight.jpg')
 
 }else{
-    img.setAttribute('src', './assets/imgPerfil.jpg')
+    img.setAttribute('src', './assets/imgPerfil.png')
 }
 }
 //SE TIVER SEM O LIGHT MODE, MANTER A IMAGEM NORMAAL
